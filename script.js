fd0c70d6ba6a2633b89cbb04ba894587ba8e33ba
Duy Anh <68810223+123rider@users.noreply.github.com>
2022-10-23 08:39:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const studentInfo = [
+  ["Bùi Xuân Huấn", 38, "THCS Yên Hòa"],
+  ["Bùi Quốc Thái", 38, "THCS Quang Trung"]
+]
+
+let student = document.getElementById("student")
+
+for (let i = 0 i<studentInfo) {
+
+}
